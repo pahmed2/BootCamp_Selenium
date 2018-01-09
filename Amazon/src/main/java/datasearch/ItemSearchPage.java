1e95e7bdc34c5480952590cfe0387d6ca6e54fca
mhasan90 <mahmudbd90@gmail.com>
2018-01-09 18:05:07 -0500

    Create DataReader Test Cases

---
+++ Amazon/src/main/java/datasearch/ItemSearchPage.java
@@ -0,0 +1,49 @@
+package datasearch;
+
+import base.CommonAPI;
+import utility.DataReader;
+import java.io.IOException;
+
+public class ItemSearchPage extends CommonAPI {
+    DataReader dtr = new DataReader();
+    public String[] getData(String fileName) throws IOException {
+        String path = "../Amazon/data/" + fileName;
+        String [] output = dtr.colReader(path,2);
+        return output;
+    }
+    public String[] getAssretData(String fileName) throws IOException {
+        String path = "../Amazon/data/" + fileName;
+        String [] output = dtr.colReader(path,3);
+        return output;
+    }
+    // AMZ_TS4: Use test data to search multiple items
+    // AMZ_TS4_TC1:  Search multiple items by name from a xls file
+    public String[] searchItemByName(String fileName) throws IOException, InterruptedException {
+        String[] items = getData(fileName);
+        String[] actual = new String[items.length];
+        for (int i=0; i<items.length; i++){
+            sleepFor(1);
+            typeByIdNEnter("twotabsearchtextbox", items[i]);
+            sleepFor(1);
+            actual[i] = getCurrentPageTitle();
+            clearInputFieldById("twotabsearchtextbox");
+            sleepFor(1);
+        }
+        return actual;
+    }
+    // AMZ_TS4_TC2:  Search multiple items by bar-code from a xls file
+    public String[] searchItemByBarCode(String fileName) throws IOException, InterruptedException {
+        String[] items = getData(fileName);
+        String[] assertItems = getAssretData(fileName);
+        String[] actual = new String[items.length];
+        for (int i=0; i<items.length; i++){
+            sleepFor(1);
+            typeByIdNEnter("twotabsearchtextbox", items[i]);
+            sleepFor(1);
+            actual[i] = getTextByXpath("//*[text()='"+assertItems[i]+"']");
+            clearInputFieldById("twotabsearchtextbox");
+            sleepFor(1);
+        }
+        return actual;
+    }
+}
---
+++ Amazon/src/test/java/datasearch/TestItemSearchPage.java
@@ -0,0 +1,34 @@
+package datasearch;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import reporting.TestLogger;
+import java.io.IOException;
+
+public class TestItemSearchPage extends ItemSearchPage {
+    // AMZ_TS4: Use test data to search multiple items
+    // AMZ_TS4_TC1: Search multiple items by name from a xls file
+    @Test
+    public void amzTS04TC01() throws IOException, InterruptedException {
+        TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object(){}.getClass().getEnclosingMethod().getName()));
+        String[] expectedItems = getAssretData("SearchItemByName.xls");
+        String[] actualItems = searchItemByName("SearchItemByName.xls");
+        for (int i=0; i<actualItems.length; i++){
+            Assert.assertTrue(actualItems[i].contains(expectedItems[i]));
+            System.out.println(expectedItems[i] + ": Search - Passed");
+        }
+        System.out.println("AMZ TS4 TC1 Passed");
+    }
+    // AMZ_TS4_TC2:  Search multiple items by bar-code from a xls file
+    @Test
+    public void amzTS04TC02() throws IOException, InterruptedException {
+        TestLogger.log(getClass().getSimpleName() + ": " + convertToString(new Object(){}.getClass().getEnclosingMethod().getName()));
+        String[] expectedItems = getAssretData("SearchItemByBarCode.xls");
+        String[] actualItems = searchItemByBarCode("SearchItemByBarCode.xls");
+        for (int i=0; i<actualItems.length; i++){
+            Assert.assertTrue(actualItems[i].contains(expectedItems[i]));
+            System.out.println(expectedItems[i] + ": Search - Passed");
+        }
+        System.out.println("AMZ TS4 TC2 Passed");
+    }
+}
