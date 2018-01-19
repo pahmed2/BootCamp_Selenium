2880e6c2fedd0fb2abd281cafe8ed8855934bc32
Mosharafh <h0mosharaf@gmail.com>
2018-01-19 11:30:52 -0500

    Update cnn module

--- Cnn/src/main/java/pages/HomePage.java
+++ Cnn/src/main/java/pages/HomePage.java
@@ -18,4 +18,57 @@ public class HomePage extends CommonAPI {
         String status = getCurrentPageUrl();
         return status;
     }
+    // CNN_TS1_TC3: Language set in english
+    public String isLanguageEnglish(){
+        String language = getTextByXpath("//*[@id=\"nav\"]/div[2]/div[3]/div[1]");
+        return language;
+    }
+
+    //CNN_TS1_TC4: US button is clickable
+    public String isUSClickable(){
+        clickByCss("#nav > div.nav__container > div.nav-menu-links > a:nth-child(1)");
+        String result = getCurrentPageTitle();
+        return result;
+    }
+    //CNN_TS1_TC5: World button is clickable
+    public String isWorldClickable(){
+        clickByCss("#nav > div.nav__container > div.nav-menu-links > a:nth-child(2)");
+        String result = getCurrentPageTitle();
+        return result;
+    }
+    //CNN_TS1_TC6: Politics button is clickable
+    public String isPoliticsClickable(){
+    clickByCss("#nav > div.nav__container > div.nav-menu-links > a:nth-child(3)");
+        String result = getCurrentPageTitle();
+        return result;
+    }
+    //CNN_TS1_TC7: Money button is clickable
+    public String isMoneyClickable() throws InterruptedException {
+        sleepFor(2);
+        clickByXpath("//*[@id=\"nav\"]/div[2]/div[2]/a[4]");
+        sleepFor(2);
+        String result = getCurrentPageTitle();
+        return result;
+    }
+    //CNN_TS1_TC8: Opinion button is clickable
+    public String isOpinionClickable(){
+        clickByCss("#nav > div.nav__container > div.nav-menu-links > a:nth-child(5)");
+        String result = getCurrentPageTitle();
+        return result;
+    }
+    //CNN_TS1_TC9: Health button is clickable
+    public String isHealthClickable(){
+        clickByCss("#nav > div.nav__container > div.nav-menu-links > a:nth-child(6)");
+        String result = getCurrentPageTitle();
+        return result;
+    }
+    //CNN_TS1_TC10: Tech button is clickable
+    public String isTechClickable() throws InterruptedException {
+        sleepFor(1);
+        clickByCss("#nav > div.nav__container > div.nav-menu-links > a:nth-child(8)");
+        sleepFor(1);
+        String result = getCurrentPageTitle();
+        return result;
+    }
+
 }
